595ed1fcf50b0c19c238a76e78e72b1f18dea6ac
agusgermi <agerminario@fi.uba.ar>
2024-09-03 06:50:56 -0300

    feat: Add error handling for invalid syntax in command line arguments

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+pub enum ErrorType {
+    InvalidTable,
+    InvalidColumn,
+    InvalidSyntax,
+    Error, // to handle more general errors
+}
+
+pub fn print_error(error_type: ErrorType, description: &str){
+    match error_type {
+        ErrorType::InvalidTable => eprintln!("[INVALID_TABLE]: [{}]", description),
+        ErrorType::InvalidColumn => eprintln!("[INVALID_COLUMN]: [{}]", description),
+        ErrorType::InvalidSyntax => eprintln!("[INVALID_SYNTAX]: [{}]", description),
+        ErrorType::Error => eprintln!("[ERROR]: [{}]", description),
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod error;
+
+
+use std::env; //to get the arguments from the command line
+use error::{ErrorType, print_error};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); 
+
+    if args.len() < 4{
+        let error_description = "Uso: cargo run -- ruta/a/tablas \"<consulta>\"";
+        let error = ErrorType::InvalidSyntax;
+        print_error(error, error_description);
+        return ();
+    }
+    println!("Hola");
 }
