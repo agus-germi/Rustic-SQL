827f73483b6be3cdf94a2f1846aa559b20f13d98
agusgermi <agerminario@fi.uba.ar>
2024-09-03 11:58:42 -0300

    feat: at least it filters by one condition

--- src/extras.rs
+++ src/extras.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+pub enum Value {
+    Int(i32),
+    Str(String),
+}
+
+pub fn cast_to_value(s: &str) -> Value {
+    if let Ok(int_value) = s.parse::<i32>() {
+        Value::Int(int_value)
+    } else {
+        Value::Str(s.to_string())
+    }
+}
+
+
+
+pub fn get_int_value(value: &Value) -> Option<i32> {
+    match value {
+        Value::Int(v) => Some(*v),
+        Value::Str(_) => None,
+    }
+}
+  
+pub fn get_str_value(value: &Value) -> Option<String> {
+    match value {
+        Value::Int(_) => None,
+        Value::Str(v) => Some(v.to_string().to_lowercase()),
+    }
+}
 
 pub fn get_columns(parsed_query: &Vec<String>) -> Vec<String> {
     let mut columns = Vec::new();
@@ -6,7 +35,6 @@ pub fn get_columns(parsed_query: &Vec<String>) -> Vec<String> {
         columns.push(parsed_query[index].to_string());
         index += 1;
     }
-    println!("COLUMNS: {:?}", columns);
     columns
 
 }
@@ -24,4 +52,15 @@ pub fn get_condition_columns(parsed_query: &Vec<String>) -> Vec<String> {
     println!("COND: {:?}", condition_columns);
     condition_columns
     
+}
+
+pub fn get_column_index(headers:&Vec<&str>, column_name: &str) -> usize {
+    let mut index = 0;
+    for header in headers {
+        if *header == column_name {
+            return index;
+        }
+        index += 1;
+    }
+    index
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ mod query_parser;
 mod extras;
 
 
-use std::{env, fs::File, io::{self, BufRead}}; //to get the arguments from the command line
+use std::{env, f32::consts::E, fs::File, io::{self, BufRead}, result}; //to get the arguments from the command line
 use error::{ErrorType, print_error};
+use extras::{cast_to_value, get_column_index, get_int_value, get_str_value, Value};
 use query_parser::{parse_query, Query, SelectParser, SelectQuery};
 
 #[derive(Debug)]
@@ -33,34 +34,109 @@ fn main() {
 
     println!("Hola");
 }
+// -- MINI FILTER FUNCTION --
+trait Operations {
+    fn apply_operation(&self, value1: Value, value2: Value) -> bool;  
+ }
+ struct EqualOperator;
+ struct GreaterThanOperator;
+ struct LessThanOperator;
+ 
+ 
+ impl Operations for EqualOperator {
+   fn apply_operation(&self, value1: Value, value2: Value) -> bool {
+     let int_value1 = get_int_value(&value1);
+     let int_value2 = get_int_value(&value2);
+     let str_value1 = get_str_value(&value1);
+     let str_value2 = get_str_value(&value2);
+     match (int_value1, int_value2, str_value1, str_value2) {
+       (Some(i1), Some(i2), _, _) => i1 == i2,
+       (_, _, Some(s1), Some(s2)) => s1 == s2,
+       _ => false,
+     }
+ }}
+ 
+ impl Operations for GreaterThanOperator {
+   fn apply_operation(&self, value1: Value, value2: Value) -> bool {
+     let int_value1 = get_int_value(&value1);
+     let int_value2 = get_int_value(&value2);
+     let str_value1 = get_str_value(&value1);
+     let str_value2 = get_str_value(&value2);
+     match (int_value1, int_value2, str_value1, str_value2) {
+       (Some(i1), Some(i2), _, _) => i1 > i2,
+       (_, _, Some(s1), Some(s2)) => false, //FIXME: turn it into a syntax error
+       _ => false,
+     }
+   }
+ }
+ 
+ impl Operations for LessThanOperator {
+   fn apply_operation(&self, value1: Value, value2: Value) -> bool {
+     let int_value1 = get_int_value(&value1);
+     let int_value2 = get_int_value(&value2);
+     let str_value1 = get_str_value(&value1);
+     let str_value2 = get_str_value(&value2);
+     match (int_value1, int_value2, str_value1, str_value2) {
+       (Some(i1), Some(i2), _, _) => i1 < i2,
+       (_, _, Some(s1), Some(s2)) => false, //FIXME: turn it into a syntax error
+       _ => false,
+     }
+   }
+ } 
+pub fn filter(value1: Value, value2: Value, operator: &str) -> bool {
+    let operator: Box<dyn Operations> = match operator {
+        "=" => Box::new(EqualOperator),
+        ">" => Box::new(GreaterThanOperator),
+        "<" => Box::new(LessThanOperator),
+        _ => return false,
+    };
+    operator.apply_operation(value1, value2)
+}
+//  FILTER FUNCTION --
+
+pub fn filter_row(row: Vec<String>, query: &SelectQuery, headers: &Vec<&str>) -> bool{
+    let column_condition_index = get_column_index(headers, query.condition[0].as_str());
+    let column_condition_value = cast_to_value(query.condition[2].as_str());
+    let operator = query.condition[1].as_str();
+    let value = cast_to_value(&row[column_condition_index]);
+
+    filter(value, column_condition_value, operator)
+}
 
 // -- EXECUTE FUNCTION --
 
-pub fn select(query: SelectQuery) {
-    println!("{}", query.table_name.trim());
-    let path = format!("{}.csv", query.table_name);
-    println!("{}", path);
-    if let Ok(file) = File::open(&path) {
-        let reader = io::BufReader::new(file);
-        for line in reader.lines() {
-            match line {
-                Ok(line_content) => {
-                    // Process each line here
-                    println!("{}", line_content);
-                }
-                Err(e) => {
-                    let error = ErrorType::InvalidTable;
-                    print_error(error, "Error al leer el archivo");
-                    return;
-                }
+pub fn select(query: SelectQuery) -> Result<(), ErrorType>{
+    let relative_path = format!("{}.csv", query.table_name);
+
+
+    if let Ok(file) = File::open(&relative_path) {
+        let mut reader: io::BufReader<File> = io::BufReader::new(file);
+        let mut header: String = String::new();
+
+        //Obtengo los headers
+        reader.read_line(&mut header);
+        let header = header.trim();
+        let headers: Vec<&str> = header.split(',').collect();
+
+        let mut lines = reader.lines();
+        lines.next(); // me salteo header
+        let mut result: Vec<String> = Vec::new();
+        for line in lines{
+            if let Ok(line) = line {
+                let values: Vec<String> = line.split(",").map(|s| s.to_string()).collect();
+                if filter_row(values, &query, &headers){
+                    result.push(line);
+                };
+            } else {
+                // TODO: handle error
             }
         }
     } else {
-        let error = ErrorType::InvalidTable;
-        print_error(error, "Error al leer el archivo");
-        return;
-    }
+        print_error(ErrorType::InvalidTable, "No se pudo abrir el archivo");
+        return Err(ErrorType::InvalidTable);    }
+    Ok(())
 }
+
 pub fn execute(query: Query) {
     match query {
         Query::Select(select_query) => {
@@ -72,3 +148,11 @@ pub fn execute(query: Query) {
     }
 }
 
+fn get_headers(reader : &mut io::BufReader<File>) -> Vec<String> {
+    let mut header = String::new();
+    reader.read_line(&mut header);
+    let header = header.trim();
+    let headers: Vec<String> = header.split(',').map(|s| s.to_string()).collect();
+    headers
+    
+}
